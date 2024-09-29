52f88a84fb72d1413eb2a0c8d3bfd7b0a646bac3
Stephen SooHoo <stephen+github@soohoo.io>
2024-09-29 17:39:36 -0500

    instrument the CLI to set the version

--- cmd/root.go
+++ cmd/root.go
@@ -22,16 +22,20 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
+var (
+	version = "{version}"
+	commit  = "{commit}"
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "cobra-cli-version-on-build",
+	Use:   "cobra-cli-example",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -47,6 +51,7 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	rootCmd.Version = fmt.Sprintf("%s (%s)", version, commit)
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
@@ -64,5 +69,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
